ba1cad3c304ac6bdef2a040b806bcab27b42b9e9
zhangcl <zhangcl@yunyoufreight.com>
2024-04-02 22:02:42 +0800

    lesson_2

--- src/main/java/org/greentree/core/PropertyValue.java
+++ src/main/java/org/greentree/core/PropertyValue.java
@@ -1,5 +1,8 @@
 package org.greentree.core;
 
+import lombok.Data;
+
+@Data
 public class PropertyValue {
     private final String name;
     private final Object value;
--- src/main/java/org/greentree/core/PropertyValues.java
+++ src/main/java/org/greentree/core/PropertyValues.java
@@ -1,4 +1,60 @@
 package org.greentree.core;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class PropertyValues {
+
+    private final List<PropertyValue> propertyValueList;
+
+    public PropertyValues() {
+        this.propertyValueList = new ArrayList<>(0);
+    }
+
+    public List<PropertyValue> getPropertyValueList() {
+        return this.propertyValueList;
+    }
+
+    public int size() {
+        return propertyValueList.size();
+    }
+
+    public void addPropertyValue(PropertyValue pv) {
+        this.propertyValueList.add(pv);
+    }
+
+    public void addPropertyValue(String propertyName, Object propertyValue) {
+        this.addPropertyValue(new PropertyValue(propertyName, propertyValue));
+    }
+
+    public void removePropertyValue(PropertyValue pv) {
+        this.propertyValueList.remove(pv);
+    }
+
+    public void removePropertyValue(String propertyName) {
+        this.propertyValueList.remove(getPropertyValue(propertyName));
+    }
+
+    public PropertyValue getPropertyValue(String propertyName) {
+        for (PropertyValue pv : propertyValueList) {
+            if (pv.getName().equals(propertyName)) {
+                return pv;
+            }
+        }
+        return null;
+    }
+
+    public Object get(String propertyName) {
+        PropertyValue propertyValue = this.getPropertyValue(propertyName);
+        return propertyValue == null ? null : propertyValue.getValue();
+    }
+
+    public boolean contains(String propertyName) {
+        return this.getPropertyValue(propertyName) != null;
+    }
+
+    public boolean isEmpty() {
+        return this.propertyValueList.isEmpty();
+    }
+
 }
